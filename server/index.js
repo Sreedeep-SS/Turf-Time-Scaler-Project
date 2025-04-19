e68d8935520c683104d648e6f6d6472a97142a43
Sreedeep-SS <sreedeep.dfd@gmail.com>
2025-04-20 04:29:47 +0530

    Add: .gitignore

--- server/index.js
+++ server/index.js
@@ -5,13 +5,10 @@ const app = express()
 
 const PORT = 5003
 
-mongoose.connect('mongodb+srv://sreedeepdeepu20122000:p1jGpmw77hwqINkW@cluster0.ageq8.mongodb.net/turfs?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect('')
 .then(() => console.log("MongoDB connected"))
 .catch((err) => console.log(err))
 
 app.listen(PORT, () =>
     console.log('Server Started')
 )
-
-
-// MONGO_URI = "mongodb+srv://sreedeepdeepu20122000:p1jGpmw77hwqINkW@cluster0.ageq8.mongodb.net/turfs?retryWrites=true&w=majority&appName=Cluster0"
