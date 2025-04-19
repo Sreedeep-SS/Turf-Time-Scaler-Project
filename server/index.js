35fada4ac1ca3831d5da460aae7c1f1958749d63
Sreedeep-SS <sreedeep.dfd@gmail.com>
2025-04-20 04:35:25 +0530

    UPDATE: mongoose connect using dotenv

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const mongoose = require('mongoose')
+require('dotenv').config({path: '../.env'});
 
 const app = express()
 
 const PORT = 5003
 
-mongoose.connect('')
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB connected"))
 .catch((err) => console.log(err))
 
